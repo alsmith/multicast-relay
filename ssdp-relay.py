9034545e77f78c64d612fdba88e9e7bf5a68ca19
Al Smith <ajs@riverbed.com>
2017-12-21 17:23:38 +0100

    Use just one socket to receive traffic and bind it to multiple IFs. Adjust the mechanism where by the outbound interface is chosen. Comparing socket objects proved to be flawed.

--- ssdp-relay.py
+++ ssdp-relay.py
@@ -13,21 +13,6 @@ import sys
 def log():
     return logging.getLogger(__file__)
 
-def get_interface(ifname):
-    """
-    Truly horrible way to get the interface addresses, given an interface name.
-    http://stackoverflow.com/questions/11735821/python-get-localhost-ip
-    """
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        arg = struct.pack('256s', ifname[:15])
-        mac = fcntl.ioctl(s.fileno(), 0x8927, arg)[18:24]
-        ip = socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, arg)[20:24])
-        return (mac, ip)
-    except IOError:
-        print 'Error getting information about interface %s' % ifname
-        sys.exit(1)
-
 class SSDP():
     def __init__(self, interfaces, verbose):
         self.verbose = verbose
@@ -37,26 +22,30 @@ class SSDP():
         self.mac = '\x01\x00\x5e\x7f\xff\xfa'
         self.ethertype = '\x08\x00'
 
-        for interface in interfaces:
-            (mac, ip) = get_interface(interface)
+        # Receiving socket
+        r = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
+        r.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-            # Receiving socket
-            r = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
-            r.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        for interface in interfaces:
+            mac, ip, netmask = self.getInterface(interface)
+
+            # Add this interface to the receiving socket's list.
             mreq = struct.pack('4s4s', socket.inet_aton(self.addr), socket.inet_aton(ip))
             r.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-            r.bind((self.addr, self.port))
 
             # Sending socket
-            s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-            s.bind((interface, 0))
+            tx = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+            tx.bind((interface, 0))
 
-            self.interfaces[interface] = {'addr': ip, 'mac': mac, 'recv': r, 'send': s}
+            self.interfaces[interface] = {'addr': ip, 'mac': mac, 'netmask': netmask, 'tx': tx}
+
+        r.bind((self.addr, self.port))
+        self.mcast_listen = r
 
     def loop(self):
         recentChecksums = []
         while True:
-            inputready, _, _ = select.select(map(lambda interface: self.interfaces[interface]['recv'], self.interfaces), [], [])
+            inputready, _, _ = select.select([self.mcast_listen], [], [])
             for s in inputready:
                 data, addr = s.recvfrom(10240)
 
@@ -67,16 +56,58 @@ class SSDP():
                 if ipChecksum in recentChecksums:
                     continue
                 recentChecksums.append(ipChecksum)
-                if (len(recentChecksums) > 16):
+                if len(recentChecksums) > 16:
                     recentChecksums = recentChecksums[1:]
+
+                receivingInterface = 'unknown'
+                for interface in self.interfaces:
+                    if self.onNetwork(addr[0], self.interfaces[interface]['addr'], self.interfaces[interface]['netmask']):
+                        receivingInterface = interface
 
                 for interface in self.interfaces:
                     # Re-transmit on all other interfaces than on the interface that we received this multicast packet from...
-                    if self.interfaces[interface]['recv'] != s:
+                    if not self.onNetwork(addr[0], self.interfaces[interface]['addr'], self.interfaces[interface]['netmask']):
                         packet = self.mac+self.interfaces[interface]['mac']+self.ethertype+data
-                        self.interfaces[interface]['send'].send(packet)
+                        self.interfaces[interface]['tx'].send(packet)
                         if self.verbose:
-                            log().info('Relayed %s byte%s from %s to %s.' % (len(data), len(data) != 1 and 's' or '', addr[0], interface))
+                            log().info('Relayed %s byte%s from %s on %s to %s via %s.' % (len(data), len(data) != 1 and 's' or '', addr[0], receivingInterface, interface, self.interfaces[interface]['addr']))
+
+    @staticmethod
+    def getInterface(ifname):
+        """
+        Truly horrible way to get the interface addresses, given an interface name.
+        http://stackoverflow.com/questions/11735821/python-get-localhost-ip
+        """
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            arg = struct.pack('256s', ifname[:15])
+
+            mac = fcntl.ioctl(s.fileno(), 0x8927, arg)[18:24]
+            ip = socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, arg)[20:24])
+            netmask = socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x891b, arg)[20:24])
+            return (mac, ip, netmask)
+        except IOError:
+            print 'Error getting information about interface %s' % ifname
+            sys.exit(1)
+
+    @staticmethod
+    def ip2long(ip):
+        """
+        Given an IP address (or netmask) turn it into an unsigned long.
+        """
+        packedIP = socket.inet_aton(ip)
+        return struct.unpack('!L', packedIP)[0]
+
+    @staticmethod
+    def onNetwork(ip, network, netmask):
+        """
+        Given an IP address and a network/netmask tuple, work out
+        if that IP address is on that network.
+        """
+        ipL = SSDP.ip2long(ip)
+        networkL = SSDP.ip2long(network)
+        netmaskL = SSDP.ip2long(netmask)
+        return (ipL & netmaskL) == (networkL & netmaskL)
 
 def main():
     parser = argparse.ArgumentParser()
